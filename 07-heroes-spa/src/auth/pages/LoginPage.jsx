2b26aaaf18132ce0fda67dcfc56623abb4fd836a
Jackson <jramon@rcbglobal.int>
2024-03-22 16:49:12 -0500

    Add Journal App project initialization, issues with urls of hero app

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
+
+  const { login } = useContext( AuthContext )
 
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/', {
+
+    const lastPath = localStorage.getItem('lastPath') || '/';
+    
+    login( 'Jackson Ramon' )
+
+    navigate( lastPath, {
       replace: true //  Para eliminar el historial y no permitir regresar atras
     })
   };
--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
+import { useContext } from 'react';
 
 
 export const Navbar = () => {
 
+    const { user, logout } = useContext( AuthContext );
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout();
+
         navigate('/login', {
             replace: true
         });
@@ -54,7 +59,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
                     
                     <span className="nav-item nav-link text-primary">
-                        Jackson
+                        { user?.name }
                     </span>
 
                     <button
